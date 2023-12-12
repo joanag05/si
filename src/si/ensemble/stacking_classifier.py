5dc82ac2e35500c67cb66bd52ae5c3b99e2a9fac
joanag05 <pg49835@alunos.uminho.pt>
2023-12-12 00:02:53 +0000

    [Aula 7] Parte I

--- src/si/ensemble/stacking_classifier.py
+++ src/si/ensemble/stacking_classifier.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from typing import List
 from si.data.dataset import Dataset
 from si.metrics.accuracy import accuracy
 
@@ -7,9 +7,15 @@ from si.metrics.accuracy import accuracy
 
 class StackingClassifier:
     def __init__(self, models: list, final_model):
+        """
+        Initialize the StackingClassifier with a list of models and a final model.
+        :param models: List of models to be stacked.
+        :param final_model: Final model to make predictions.
+        """
         # parameters
         self.models = models
         self.final_model = final_model
+    
 
     def fit(self, dataset: Dataset) -> 'StackingClassifier':
         """
@@ -56,3 +62,32 @@ class StackingClassifier:
         score = accuracy(dataset.y, y_pred)
 
         return score
+    
+if __name__ == '__main__':
+    from si.io.csv_file import read_csv
+    from si.model_selection.split import stratified_train_test_split
+    from si.models.knn_classifier import KNNClassifier
+    from si.models.logistic_regression import LogisticRegression
+    from si.models.decision_tree_classifier import DecisionTreeClassifier
+
+    data = "C:\\Users\\joana\\OneDrive\\Documentos\\GitHub\\si\\datasets\\breast_bin\\breast-bin.csv"  
+    breast=read_csv(data, sep=",",features=True,label=True)
+    train_data, test_data = stratified_train_test_split(breast, test_size=0.20, random_state=42)
+
+    #knnregressor
+    knn = KNNClassifier(k=3)
+    
+    #logistic regression
+    LG=LogisticRegression(l2_penalty=1, alpha=0.001, max_iter=1000)
+
+    #decisiontreee
+    DT=DecisionTreeClassifier(min_sample_split=3, max_depth=3, mode='gini')
+
+    # Final Model (Choose a different model as the final model)
+    final_model = LogisticRegression(l2_penalty=1, alpha=0.001, max_iter=1000)
+
+    # Initialize StackingClassifier
+    models = [knn, LG, DT]
+    exercise = StackingClassifier(models, final_model)
+    exercise.fit(train_data)
+    print(exercise.score(test_data))
--- src/si/models/random_forest_classifier.py
+++ src/si/models/random_forest_classifier.py
@@ -1,14 +1,11 @@
+from typing import List, Tuple
 import numpy as np
-import numpy as np
-from collections import Counter
-from si.model_selection.split import train_test_split
-from models.decision_tree_classifier import DecisionTreeClassifier
-
+from si.data.dataset import Dataset
+from si.models.decision_tree_classifier import DecisionTreeClassifier
 
 class RandomForestClassifier:
-
-    def __init__(self, n_estimators, max_features, min_sample_split,max_depth, mode, seed):
-
+    def __init__(self, n_estimators: int, max_features: int = None, min_sample_split: int = 2,
+                 max_depth: int = None, mode: str = 'gini', seed: int = None):
         self.n_estimators = n_estimators
         self.max_features = max_features
         self.min_sample_split = min_sample_split
@@ -16,58 +13,57 @@ class RandomForestClassifier:
         self.mode = mode
         self.seed = seed
         self.trees = []
-    
-    def fit(self, X, y):
-        if self.seed:
-            np.random.seed(self.seed)
 
-        n_samples, n_features = X.shape
+    def fit(self, dataset: Dataset) -> 'RandomForestClassifier':
+        np.random.seed(self.seed)
+        n_samples, n_features = dataset.X.shape
         self.max_features = int(np.sqrt(n_features)) if self.max_features is None else self.max_features
 
         for _ in range(self.n_estimators):
-            idx = np.random.choice(n_samples, n_samples, replace=True)
-            features = np.random.choice(n_features, self.max_features, replace=False)
-
-            X_bootstrap = X[idx][:, features]
-            y_bootstrap = y[idx]
-
-            tree = DecisionTreeClassifier(max_depth=self.max_depth, min_samples_split=self.min_samples_split, criterion=self.mode)
-            tree.fit(X_bootstrap, y_bootstrap)
-
-            self.trees.append((features, tree))
+            bootstrap_indices = np.random.choice(n_samples, size=n_samples, replace=True)
+            feature_indices = np.random.choice(n_features, size=self.max_features, replace=False)
+            tree_dataset = Dataset(dataset.X[bootstrap_indices][:, feature_indices], dataset.y[bootstrap_indices])
+            
+            tree = DecisionTreeClassifier(min_sample_split=self.min_sample_split, max_depth=self.max_depth,
+                                          mode=self.mode)
+            tree.fit(tree_dataset)
+            
+            self.trees.append((feature_indices, tree))
 
         return self
-        
 
-    def predict(self, X):
+    def predict(self, dataset: Dataset) -> np.ndarray:
         predictions = []
         for features, tree in self.trees:
-            X_subset = X[:, features]
-            tree_pred = tree.predict(X_subset)
+            tree_dataset = Dataset(dataset.X[:, features], dataset.features, label=dataset.label)
+            tree_pred = tree.predict(tree_dataset)
             predictions.append(tree_pred)
 
-        predictions = np.array(predictions)
-        ensemble_preds = np.array([Counter(sample).most_common(1)[0][0] for sample in predictions.T])
-        return ensemble_preds
+        predictions = np.array(predictions).T
+        final_predictions = np.array([np.argmax(np.bincount(pred)) for pred in predictions])
+        return final_predictions
 
-    def score(self, X, y):
-        y_pred = self.predict(X)
-        accuracy = np.mean(y == y_pred)
+    def score(self, dataset: Dataset) -> float:
+        y_pred = self.predict(dataset)
+        accuracy = np.mean(y_pred == dataset.y)
         return accuracy
-    
+
+
 if __name__ == '__main__':
     from si.io.csv_file import read_csv
+    from si.model_selection.split import stratified_train_test_split
 
-    df = read_csv(r'C:\Users\joana\OneDrive\Documentos\GitHub\si\datasets\iris\iris.csv', label=True, features=True)
-    
-X_train, X_test, y_train, y_test = train_test_split(df, iris.target, test_size=0.2, random_state=42)
-
-rf = RandomForestClassifier(n_estimators=100, max_features=None, min_samples_split=2, max_depth=float('inf'), mode='gini', seed=42)
-rf.fit(X_train, y_train)
-
-test_score = rf.score(X_test, y_test)
-print(f"Test set accuracy: {test_score:.4f}")
+    data =  "C:\\Users\\joana\\OneDrive\\Documentos\\GitHub\\si\\datasets\\iris\\iris.csv"
+    data = read_csv(data, sep=",",features=True,label=True)
+   
+    train_data, test_data = stratified_train_test_split(data, test_size=0.2, random_state=42)
 
 
+    random_forest = RandomForestClassifier(n_estimators=10, max_features=None, min_sample_split=2,
+                                       max_depth=None, mode='gini', seed=42)
+    random_forest.fit(train_data)
 
-        
+    score = random_forest.score(test_data)
+    print(f"Accuracy on test set: {score}")
+    score = random_forest.score(train_data)
+    print(f"Accuracy on train set: {score}")
